2bcb7da6e51bd25f0813c3249eab7664735cc64d
jones2000 <jones_2000@163.com>
2025-12-16 14:49:58 +0800

    上传项目

---
+++ src/HQDataWorker.js
@@ -0,0 +1,115 @@
+
+importScripts("../include/hqchart/umychart.js","umychart.hqdata.worker.js")
+
+
+chrome.runtime.onInstalled.addListener(({ reason }) => 
+{
+    if (reason === 'install') 
+    {
+        
+    }
+});
+
+
+var rules = 
+{
+    removeRuleIds: [1],
+    addRules: 
+    [
+        {
+            id: 1,
+            priority: 1,
+            condition: 
+            {
+                urlFilter: 'hq.sinajs.cn/*',
+                // domains: ['pic.ibaotu.com'],
+            },
+            action: 
+            {
+                type: "modifyHeaders",
+                requestHeaders: 
+                [
+                    {
+                        header: "Referer",
+                        operation: "set",
+                        value: "https://vip.stock.finance.sina.com.cn/"
+                    },
+                ],
+            }
+        }
+    ]
+
+};
+
+modifyHeaderIfNecessary();
+
+function modifyHeaderIfNecessary() 
+{
+    chrome.declarativeNetRequest.updateDynamicRules(rules, () => 
+    {
+        if (chrome.runtime.lastError) 
+        {
+            console.error(chrome.runtime.lastError);
+        } 
+        else 
+        {
+            chrome.declarativeNetRequest.getDynamicRules(rules => console.log(rules));
+        }
+    });
+}
+
+
+
+var g_HQChartDataService=new HQChartDataService();
+g_HQChartDataService.Create();
+
+//g_HQChartDataService.SubscribePool.push({ Type:1});
+
+/*
+var subscribeItem=new HQSubscribe()
+subscribeItem.Type=1;
+subscribeItem.WindowID="dddd";
+subscribeItem.PageID="AAAA";
+subscribeItem.ArySymbol=["601006.sh","000858.sz","300750.sz"];
+*/
+
+/*
+var item=new HQRequestItem()
+item.Type=JSCHART_DATA_TYPE_ID.KLINE_MIN_DATA_ID;
+item.ArySymbol=[{ Symbol:"601006.sh" }];
+*/
+
+
+/*
+var item=new HQRequestItem()
+item.Type=JSCHART_DATA_TYPE_ID.MINUTE_DATA_ID;
+item.ArySymbol=[{ Symbol:"601006.sh" },{ Symbol:"000858.sz"} ,{ Symbol:"300750.sz"} ];
+*/
+
+/*
+var item=new HQRequestItem()
+item.Type=JSCHART_DATA_TYPE_ID.BASE_DATA_V2_ID;
+//item.ArySymbol=[{ Symbol:"601006.sh" },{ Symbol:"000858.sz"} ,{ Symbol:"300750.sz"} ];
+item.ArySymbol=[{ Symbol:"600000.sh", Fields:{ShareCapital:true} }];
+*/
+
+
+/*
+var item=new HQRequestItem()
+item.Type=JSCHART_DATA_TYPE_ID.SHARE_ID;
+item.ArySymbol=[{ Symbol:"600000.sh", Count:1 }];
+*/
+
+/*
+var item=new HQRequestItem()
+item.Type=JSCHART_DATA_TYPE_ID.DETAIL_FULL_DATA_ID;
+item.ArySymbol=[{ Symbol:"600000.sh" }];
+*/
+
+
+var item=new HQRequestItem()
+item.Type=JSCHART_DATA_TYPE_ID.DETAIL_PRICE_ID;
+item.ArySymbol=[{ Symbol:"600000.sh" }];
+
+
+g_HQChartDataService.RequestData(item);
